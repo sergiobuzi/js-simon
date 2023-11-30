fbc89b5fc38d9ab67379a092fa820556dc7ed06c
SergioBuzi <sergio.buzi97@gmail.com>
2023-11-30 17:28:45 +0100

    script: adding timer and numberArray

--- script.js
+++ script.js
@@ -4,51 +4,51 @@
 
 
 
-// Dichiaro la variabile del bottone e la collego al file html
-const SendButton = document.getElementById("send");
-
+// Dichiaro la variabile del bottone per iniziare il gioco e la collego al file html
+const startButton = document.getElementById("start");
 // creo la varibile che si collega i nuemeri dell'array al file
-const numberToShow = document.getElementById("numeri")
+const numberToShow = document.getElementById("numeri");
+//dichiaro la variabile dell'input per inserire i numeri
+const inputNumeri = document.getElementById("text");
+//dichiaro la variabile del bottone per mandare i numeri
+const sendButton = document.getElementById("send");
 
 //creo la variabile del timer e la setto su 30
 let seconds = 30;
-
 //creo un array che generi 5 numeri randomici da 1 a 10
 let listaNumeri = [];
 console.log(listaNumeri);
 
 
-//creo un ciclo che mi generi 5 nuemeri randomici da 1 a 10
-for (let i = 0; i < 5; i++) {
-    listaNumeri.push(getRandomNumMinMax(1, 10));
-    
-}
-
-//mostro i numeri dell array a schermo tramite l' h2
-numberToShow.append(listaNumeri);
-
 
 //al bottone assegno la funzione click
-SendButton.addEventListener("click",
+startButton.addEventListener("click",
     
     //faccio partire un timer di 30 secondi 
     function(){
-        const clock = setInterval(
-        function(){
+        setTimeout(hide, seconds * 100);
+
+        //creo un ciclo che mi generi 5 nuemeri randomici da 1 a 10
+        for (let i = 0; i < 5; i++) {
+        listaNumeri.push(getRandomNumMinMax(1, 10));
         }
 
+        //nascondo il pulsante send
+        startButton.classList.add("hidden");
 
-
-
-
-        )
-
-
+        //mostro i numeri dell array a schermo tramite l' h2
+        numberToShow.append(listaNumeri);
+    
     }
-)
+);
 
 
-
+//funzione che nasconda i numeri dopo 30 secondi
+function hide (){
+    numberToShow.classList.add("hidden");
+    sendButton.classList.add("show");
+    inputNumeri.classList.add("show");
+}
 
 
 
@@ -57,3 +57,4 @@ SendButton.addEventListener("click",
 function getRandomNumMinMax(min, max) {
     return Math.floor(Math.random()* (max - min + 1)) + min;
 }
+
